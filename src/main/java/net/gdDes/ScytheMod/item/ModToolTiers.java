8de036033557af50a1fb67d186b2f2f023ce9492
gdDes <alonzo.ayres@gmail.com>
2023-11-09 10:26:11 -0500

    Fixed issue causing pickaxes to not work.

--- src/main/java/net/gdDes/ScytheMod/item/ModToolTiers.java
+++ src/main/java/net/gdDes/ScytheMod/item/ModToolTiers.java
@@ -7,13 +7,14 @@ import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.Tiers;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.common.ForgeTier;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.TierSortingRegistry;
 
 import java.util.List;
 
 public class ModToolTiers {
      public static final Tier SCYTHE = TierSortingRegistry.registerTier(
-            new ForgeTier(5, 4000, 5f, 5f, 40, ModTags.Blocks.NEEDS_SCYTHE,
+            new ForgeTier(5, 4000, 5f, 5f, 40, Tags.Blocks.NEEDS_NETHERITE_TOOL,
                     () -> Ingredient.of(ModItems.CORRUPT_INGOT.get())),
              new ResourceLocation(ScytheMod.MOD_ID, "scythe"), List.of(Tiers.NETHERITE), List.of());
 }
